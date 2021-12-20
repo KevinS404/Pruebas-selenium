7dcfa3288929f887cb0cbcde64804741eede1523
kevin <silva>
2021-12-20 20:37:27 -0300

    Prrebas de selenium completas

--- test11.java
+++ test11.java
@@ -15,7 +15,7 @@ public class test11 {
     private WebDriver driver;
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\sebastian\\Desktop\\Pruebas\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","E:\\Desktop\\ramos\\MINGESO\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://automationpractice.com/index.php");
         driver.manage().window().maximize();
@@ -23,21 +23,21 @@ public class test11 {
     }
     @After
     public void tearDown() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(5);
         driver.quit();
 
     }
-
+    //Contact us exitoso, este caso simula una prueba para la funcionalidad del contact us cuando funciona
     @Test
-    public void test1() {
+    public void test1() throws InterruptedException {
         driver.get("http://automationpractice.com/index.php");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
         driver.findElement(By.linkText("Contact us")).click();
         driver.findElement(By.id("id_contact")).click();
         {
             WebElement dropdown = driver.findElement(By.id("id_contact"));
             dropdown.findElement(By.xpath("//option[. = 'Customer service']")).click();
         }
+        Thread.sleep(3000);
         driver.findElement(By.cssSelector(".form-group:nth-child(5)")).click();
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).sendKeys("mingeso@gmail.com");
@@ -45,30 +45,33 @@ public class test11 {
         driver.findElement(By.id("id_order")).sendKeys("141414");
         driver.findElement(By.id("message")).click();
         driver.findElement(By.id("message")).sendKeys("Problema");
+        Thread.sleep(3000);
         driver.findElement(By.cssSelector("#submitMessage > span")).click();
     }
 
+    //error de contact us, este caso simula el error del contact us cuando no se incluye un mensaje
     @Test
-    public void test2() {
+    public void test2() throws InterruptedException{
         driver.get("http://automationpractice.com/index.php");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
         driver.findElement(By.linkText("Contact us")).click();
         driver.findElement(By.id("id_contact")).click();
         {
             WebElement dropdown = driver.findElement(By.id("id_contact"));
             dropdown.findElement(By.xpath("//option[. = 'Customer service']")).click();
         }
+        Thread.sleep(3000);
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).sendKeys("mingeso@gmail.com");
         driver.findElement(By.id("id_order")).click();
         driver.findElement(By.id("id_order")).sendKeys("141414");
+        Thread.sleep(3000);
         driver.findElement(By.cssSelector("#submitMessage > span")).click();
     }
 
+    //re ordenar un pedido
     @Test
-    public void test3() {
+    public void test3() throws InterruptedException{
         driver.get("http://automationpractice.com/index.php");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
         driver.findElement(By.linkText("Sign in")).click();
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).sendKeys("mingeso@gmail.com");
@@ -80,35 +83,41 @@ public class test11 {
         driver.findElement(By.cssSelector(".standard-checkout > span")).click();
         driver.findElement(By.cssSelector(".button:nth-child(4) > span")).click();
         driver.findElement(By.id("cgv")).click();
+        Thread.sleep(2000);
         driver.findElement(By.cssSelector(".standard-checkout > span")).click();
         driver.findElement(By.linkText("Pay by bank wire (order processing will be longer)")).click();
+        Thread.sleep(2000);
         driver.findElement(By.cssSelector("#cart_navigation span")).click();
         // Falta assertion
     }
 
+    //filtro de ropa por el precio mas bajo
     @Test
-    public void test4() {
+    public void test4() throws InterruptedException{
         driver.get("http://automationpractice.com/index.php");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
         driver.findElement(By.id("search_query_top")).click();
         driver.findElement(By.id("search_query_top")).sendKeys("dress");
+        Thread.sleep(2000);
         driver.findElement(By.name("submit_search")).click();
         driver.findElement(By.id("selectProductSort")).click();
         {
             WebElement dropdown = driver.findElement(By.id("selectProductSort"));
             dropdown.findElement(By.xpath("//option[. = 'Price: Lowest first']")).click();
         }
+        Thread.sleep(2000);
     }
 
+
+    //borrar lista de deseos
     @Test
-    public void test5() {
+    public void test5() throws InterruptedException{
         driver.get("http://automationpractice.com/index.php");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
         driver.findElement(By.linkText("Sign in")).click();
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).sendKeys("mingeso@gmail.com");
         driver.findElement(By.id("passwd")).click();
         driver.findElement(By.id("passwd")).sendKeys("mingeso");
+        Thread.sleep(2000);
         driver.findElement(By.cssSelector("#SubmitLogin > span")).click();
         driver.findElement(By.cssSelector(".lnk_wishlist span")).click();
         driver.findElement(By.id("name")).click();
@@ -116,25 +125,30 @@ public class test11 {
         driver.findElement(By.cssSelector("#submitWishlist > span")).click();
         driver.findElement(By.cssSelector(".icon-remove")).click();
         assertThat(driver.switchTo().alert().getText(), is("Do you really want to delete this wishlist ?"));
+        Thread.sleep(2000);
         driver.switchTo().alert().accept();
     }
 
     @Test
+    //Login valido
     public void test6() {
 
         driver.findElement(By.linkText("Sign in")).click();
         driver.findElement(By.id("email")).sendKeys("ergeag@gmail.com");
-        driver.findElement(By.id("passwd")).sendKeys("asdf");
+        driver.findElement(By.id("passwd")).sendKeys("asdgf");
         driver.findElement(By.id("SubmitLogin")).click();
         //assertEquals("My account - My Store",driver.getTitle());
         assertEquals("http://automationpractice.com/index.php?controller=my-account",driver.getCurrentUrl());
     }
 
+
+    //prueba de registro para usuario nuevo
     @Test
     public void test7() throws InterruptedException {
         driver.findElement(By.linkText("Sign in")).click();
         Thread.sleep(2000);
-        driver.findElement(By.id("email_create")).sendKeys("mingeso3@gmail.com");
+        //ES NECESARIO CAMBIAR EL CORREO CADA VEZ QUE SE EJECUTA ESTA PRUEBA
+        driver.findElement(By.id("email_create")).sendKeys("mingeso32@gmail.com");
         driver.findElement(By.id("SubmitCreate")).click();
         Thread.sleep(3000);
         driver.findElement(By.id("id_gender1")).click();
@@ -181,6 +195,7 @@ public class test11 {
         assertEquals("My account - My store",driver.getTitle());
     }
 
+    //Prueba para registrarse con un correo ya creado
     @Test
     public void test8() throws InterruptedException {
         driver.findElement(By.linkText("Sign in")).click();
@@ -191,6 +206,8 @@ public class test11 {
         assertTrue(driver.findElement(By.id("create_account_error")).isDisplayed());
     }
 
+
+    //Prueba para tratar de añadir ropa a una lista de deseos sin estar logeado
     @Test
     public void test9() throws InterruptedException {
         driver.findElement(By.linkText("Women")).click();
@@ -198,13 +215,14 @@ public class test11 {
         driver.findElement(By.linkText("Blouse")).click();
         Thread.sleep(2000);
         driver.findElement(By.id("wishlist_button")).click();
-        Thread.sleep(7000);
+        Thread.sleep(4000);
         assertTrue(driver.findElement(By.className("fancybox-error")).isDisplayed());
 
 
 
     }
 
+    //Prueba para pagar sin haber estado logeado previamente
     @Test
     public void test10() throws InterruptedException {
         driver.findElement(By.linkText("Women")).click();
@@ -234,6 +252,8 @@ public class test11 {
         assertEquals("Order confirmation - My Store",driver.getTitle());
     }
 
+
+    //Prueba para actualizar los datos del usuario
     @Test
     public void test11() throws InterruptedException {
         driver.findElement(By.linkText("Sign in")).click();
@@ -251,6 +271,17 @@ public class test11 {
 
 
 
+    }
+
+    @Test
+    //Login invalido
+    public void test12() {
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.id("email")).sendKeys("ergeag@gmail.com");
+        driver.findElement(By.id("passwd")).sendKeys("asdg");
+        driver.findElement(By.id("SubmitLogin")).click();
+        //assertEquals("My account - My Store",driver.getTitle());
+        assertEquals("http://automationpractice.com/index.php?controller=my-account",driver.getCurrentUrl());
     }
 
 }
